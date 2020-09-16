ddf1647160bd7411db387d25aaf97ae252d8fae6
Seth Campos <camposseth04@gmail.com>
2020-09-16 12:36:04 -0700

    Updates

---
+++ precache-manifest.b2e01d9577bb70c175d2ac75961acd0f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5740b435e108d2f1f363d137a92dc4ba",
+    "url": "/seltzer-quiz/index.html"
+  },
+  {
+    "revision": "8e95cdbde088547bc915",
+    "url": "/seltzer-quiz/static/css/main.51727e1a.chunk.css"
+  },
+  {
+    "revision": "a5f5e5145d46c0979bea",
+    "url": "/seltzer-quiz/static/js/2.b0b484e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seltzer-quiz/static/js/2.b0b484e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e95cdbde088547bc915",
+    "url": "/seltzer-quiz/static/js/main.0dc1edda.chunk.js"
+  },
+  {
+    "revision": "cf526c5ca674d85542d4",
+    "url": "/seltzer-quiz/static/js/runtime-main.7798b683.js"
+  },
+  {
+    "revision": "1190ca3914177002718fe294a4263959",
+    "url": "/seltzer-quiz/static/media/apricot.1190ca39.png"
+  },
+  {
+    "revision": "043a3adb5709f66de45de804ed2beec1",
+    "url": "/seltzer-quiz/static/media/berry.043a3adb.png"
+  },
+  {
+    "revision": "b0350065d9dd3e5118ceaedaf6f63304",
+    "url": "/seltzer-quiz/static/media/coconut.b0350065.png"
+  },
+  {
+    "revision": "5a48753b0c736b74f6e89a34b0e68884",
+    "url": "/seltzer-quiz/static/media/hibiscus.5a48753b.png"
+  },
+  {
+    "revision": "3614a76d966314a781f133a92c392afd",
+    "url": "/seltzer-quiz/static/media/keylime.3614a76d.png"
+  },
+  {
+    "revision": "c4c03831235436274d06870fd77a1a5a",
+    "url": "/seltzer-quiz/static/media/lemon.c4c03831.png"
+  },
+  {
+    "revision": "00766053b4cce2a1a16ed0d90f001f0c",
+    "url": "/seltzer-quiz/static/media/lime.00766053.png"
+  },
+  {
+    "revision": "def1bb9599601cbdcbb1d4c7fe6d1241",
+    "url": "/seltzer-quiz/static/media/limoncello.def1bb95.png"
+  },
+  {
+    "revision": "5762ee7f3860e6dc8156b386c38f1d30",
+    "url": "/seltzer-quiz/static/media/mango.5762ee7f.png"
+  },
+  {
+    "revision": "9aca6dc58f66a8f282055985f0256ff8",
+    "url": "/seltzer-quiz/static/media/orange.9aca6dc5.png"
+  },
+  {
+    "revision": "47d2b149ac4771dbdce853dea365a66a",
+    "url": "/seltzer-quiz/static/media/pamplemousse.47d2b149.png"
+  },
+  {
+    "revision": "4ebca782a4695728dd90ddafb8c18182",
+    "url": "/seltzer-quiz/static/media/passionfruit.4ebca782.png"
+  },
+  {
+    "revision": "a3caaf28293d2f9d1a2ad209617a8d12",
+    "url": "/seltzer-quiz/static/media/pasteque.a3caaf28.png"
+  },
+  {
+    "revision": "a0aa2f32da38c8f98d276cdda35313fa",
+    "url": "/seltzer-quiz/static/media/peachpear.a0aa2f32.png"
+  },
+  {
+    "revision": "ce46fb79805f15fae6234473adfc3378",
+    "url": "/seltzer-quiz/static/media/razzcranberry.ce46fb79.png"
+  },
+  {
+    "revision": "87f40263fa89e6b89cd0f18a25d28c14",
+    "url": "/seltzer-quiz/static/media/tangerine.87f40263.png"
+  }
+]);
